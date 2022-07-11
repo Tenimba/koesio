012d267d92a192acf0fa550b3840e0cd8423cb72
tenimba <tenimba.sangare@epitech.eu>
2022-07-11 20:47:11 +0200

    send msg back

--- node back/server.js
+++ node back/server.js
@@ -1,8 +1,19 @@
 const http = require("http");
 const express = require("express");
 const cors = require("cors");
+const bodyParser = require("body-parser");
+const pino = require("express-pino-logger")();
 const socketIO = require("socket.io");
 const mysql = require("mysql2");
+const {Webhook} = require("discord-webhook-node");
+const { IncomingWebhook } = require("@slack/client");
+const hook = new Webhook(
+  "https://discord.com/api/webhooks/996100194372419755/fvloelnmNuYcvw3WJH5IJTtz6qBWeH9dnoECjwu_DKQx8_KM41tMMN-rhegmCC-6rV9v"
+);
+const url =
+  "https://hooks.slack.com/services/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";
+const webhook = new IncomingWebhook(url);
+
 const db = mysql.createConnection({
   user: "tenimba",
   host: "localhost",
@@ -12,7 +23,6 @@ const db = mysql.createConnection({
 
 const app = express();
 app.use(cors());
-
 const server = http.createServer(app);
 const io = socketIO(server, {
   cors: {
@@ -23,12 +33,33 @@ const io = socketIO(server, {
 const PORT = process.env.PORT || 8080;
 
 io.on("connection", (socket) => {
-  // Get nickname and channel.
 
-  // Join the user to the channel.
 
   socket.on("register", (data) => {
     console.log(data);
+    if(data.password === data.confirm){
+    db.execute(
+      "INSERT INTO users (username, password) VALUES (?, ?)",
+      [data.username, data.password],
+      (err, result) => {
+        if (err) throw err;
+        if (result.affectedRows === 1) {
+          console.log("User create in");
+         socket.emit('login', {
+            username: data.username,
+            password: data.password,
+            });
+   
+        } else {
+          console.log("user not create");
+
+        }
+      }
+    );
+    }
+  });
+
+  socket.on("login", (data) => {
     db.execute(
       "Select * from users where username = ? AND password = ?",
       [data.username, data.password],
@@ -38,20 +69,101 @@ io.on("connection", (socket) => {
           console.log("User logged in");
           socket.emit("loginsucces", {
             username: data.username,
-            
           });
         } else {
           console.log("login failed");
           socket.emit("loginechec", {
             username: "",
-         
           });
         }
       }
     );
   });
 
-  // Handle disconnect
+  socket.on("create", (data) => {
+    socket.join(data);
+    console.log("User joined the channel " + data);
+  });
+
+  socket.on("message", (data) => {
+    if (data.channel === "slack") {
+      webhook.send(data.message, function (err, res) {
+        console.log(data);
+        if (err) {
+          console.log("error", err);
+        }
+      });
+      db.execute(
+        "INSERT INTO slacks (username, message, time ) VALUES (?,?,?)",
+        [data.username, data.message, data.time],
+        (err, result) => {
+          if (err) throw err;
+          if (result) {
+            socket.emit("message_send", {
+              username: data.username,
+              channel: data.channel,
+              message: data.message,
+            });
+          }
+        }
+      );
+    } else if (data.channel === "discord") {
+      hook.send(data.message, function (err, res) {
+        console.log(data);
+        if (err) {
+          console.log("error", err);
+        }
+      });
+
+      db.execute(
+        "INSERT INTO discord (username, message, time) VALUES (?, ?, ?)",
+        [data.username, data.message, data.time],
+        (err, result) => {
+          if (err) throw err;
+          console.log("Message sent");
+          socket.emit("message_send", {
+            username: data.username,
+            channel: data.channel,
+            message: data.message,
+          });
+        }
+      );
+    }
+  });
+
+  socket.on("historique", (data) => {
+    db.execute(
+      "SELECT * FROM slacks WHERE username = ?",
+      [data.username],
+      (err, result) => {
+        console.log(result);
+        if (err) throw err;
+        if (result.length > 0) {
+          socket.emit("historique_send", {
+            username: data.username,
+            channel: "slack",
+            historique: result,
+          });
+        }
+      }
+    );
+    db.execute(
+      "SELECT * FROM discord WHERE username = ?",
+      [data.username],
+      (err, result) => {
+        console.log(result);
+        if (err) throw err;
+        if (result.length > 0) {
+          socket.emit("historique_send", {
+            username: data.username,
+            channel: "discord",
+            historique: result,
+          });
+        }
+      }
+    );
+    });                                    
+
   socket.on("disconnect", () => {});
 });
 
--- react front/src/App.js
+++ react front/src/App.js
@@ -1,22 +1,39 @@
 import React from "react";
 import socketClient from "socket.io-client";
+import "./App.css";
 const server = "http://localhost:8080";
 
 
 function App() {
   var socket = socketClient(server);
   socket.on("connect", function () {});
+  const [inscription, setInscription] = React.useState(false);
+  const [connexion, setConnexion] = React.useState(false);
+  const [menu, setMenu] = React.useState(false);
   const [username, setUsername] = React.useState("");
   const [password, setPassword] = React.useState("");
   const [confirm, setConfirm] = React.useState("");
-  const [connexion , setConnexion] = React.useState(false);
   const [message, setMessage] = React.useState("");
-  const [select , setSelect] = React.useState("");
-  const options = [
-    { value: 'slack', label: 'slack' },
-    { value: 'discord', label: 'discord' },
- 
-  ]
+  const time = new Date().getHours() + ":" + new Date().getMinutes();
+  const [text, setText] = React.useState([]);
+  const [messages, setMessages] = React.useState([]);
+  const [channel, setChannel] = React.useState("");
+  const [histo,sethisto] = React.useState(false);
+  const [historique , sethistorique] = React.useState([]);
+
+
+  const Slack = (event) => {
+    socket.emit("create", "slack");
+    setMenu(true);
+    setChannel("slack");
+  };
+  const Discord = (event) => {
+    socket.emit("create", "discord");
+    setMenu(true);
+    setChannel("discord");
+  };
+
+
   const handlePseudo = (event) => {
     event.preventDefault();
     console.log();
@@ -26,95 +43,217 @@ function App() {
       password: password,
       confirm: confirm,
     });
-
-
     socket.on("loginsucces", (data) => {
       console.log(data);
       setConnexion(true);
-    if(data.username === '') {
-      setConnexion(false);
-    }else {
-      setConnexion(true);
-      
-    }
-    if(connexion === true) {
-      console.log('connexion');
-    }    
+      if (data.username === "") {
+        setConnexion(false);
+        prompt("pseudo ou mot de passe non valide");
+      } else {
+        setConnexion(true);
+      }
+      if (connexion === true) {
+        console.log("connexion");
+      }
     });
   };
 
+  const handleconnexion = (event) => {
+    event.preventDefault();
+    console.log("login: ", username, password);
+socket.emit("login", {
+  username: username,
+  password: password,
+});
+socket.on("loginsucces", (data) => {
+  console.log(data);
+  setConnexion(true);
+  if (data.username === "") {
+    setConnexion(false);
+    prompt("login ou mot de passe incorrect");
+  } else {
+    setConnexion(true);
+  }
+  if (connexion === true) {
+    console.log("connexion");
+  }
+}
+);
+  };
+const Historique = (event) => {
+  event.preventDefault();
+  console.log("historique");
+  socket.emit("historique", {
+    username: username,
+  });
+  sethisto(true);
+
+  socket.on("historique_send", (data) => {
+    sethistorique(data.historique);
+    console.log(data);
+  }
+  );
+}
+
   const handleMessage = (event) => {
     event.preventDefault();
-    console.log(message);
-    console.log(select);
-  }
+    socket.emit("message", {
+      message: message,
+      channel: channel,
+      username: username,
+      time: time,
+    });
+    console.log(message, channel, username, time);
 
+    socket.on("message_send", (data) => {
+      console.log(data.username);
+      var messag = [
+        {
+          username: data.username,
+          channel: channel,
+          message: data.message,
+          time: time,
+        },
+      ];
+      setText(messag.concat(text));
+    });
+  };
 
-if(connexion === false){
-  return (
-    <div className="flex justify-center text-center p-5 solid">
-      <div className="username">
-        <h1>Bienvenue </h1>
-        <div className="p-8">
-          <span className="label">connexion</span>
+ 
+  if (connexion === false) {
+    return (
+      <div className="flex justify-center text-center p-5 solid">
+        <div className="inscription">
+          <h1>Bienvenue </h1>
+          <div className="p-8">
+            <span className="label">Inscription</span>
+          </div>
+          <form>
+            <label>
+              <p>Username</p>
+              <input
+                type="text"
+                onChange={(e) => setUsername(e.target.value)}
+              />
+            </label>
+            <label>
+              <p>Password</p>
+              <input
+                type="password"
+                onChange={(e) => setPassword(e.target.value)}
+              />
+            </label>
+            <label>
+              <p>confirm</p>
+              <input
+                type="password"
+                onChange={(e) => setConfirm(e.target.value)}
+              />
+            </label>
+            <button className="button" onClick={handlePseudo}>
+              Se connecter
+            </button>
+          </form>
+        </div>
+<div className="connexion">
+<span className="label">connexion</span>
+         
+          <form>
+            <label>
+              <p>Username</p>
+              <input
+                type="text"
+                onChange={(e) => setUsername(e.target.value)}
+              />
+            </label>
+            <label>
+              <p>Password</p>
+              <input
+                type="password"
+                onChange={(e) => setPassword(e.target.value)}
+              />
+            </label>
+            <button className="button" onClick={handleconnexion}>
+              Se connecter
+            </button>
+          </form>
+          </div>
+      </div>
+    );
+    
+  } else if (connexion === true && menu === false) {
+    return (
+      <div className="menu">
+        <li onClick={Slack}>Slack</li>
+        <li onClick={Discord}>Discord</li>
+      
+      </div>
+    );
+  } else if (connexion === true && menu === true && histo === false) {
+    return (
+      <div>
+        <div className="menu">
+          <li onClick={Slack}>Slack</li>
+          <li onClick={Discord}>Discord</li>
+          <li onClick={Historique}>historique</li>
+        </div>
+
+        <div className="text-center name">
+          <em>
+            Bienvenue {username} sur {channel}{" "}
+          </em>
+        </div>
+
+        <div >
+          <div className="border">
+            {text.map((message, index) => (
+              <div key={index}>
+                <div> user :{message.username} </div>
+                <div> message : {message.message} </div>
+                <div> envoyer a {message.time} </div>
+                <div> envoyer vers : {message.channel} </div>
+                <br />
+              </div>
+            ))}
+          </div>
         </div>
         <form>
           <label>
-            <p>Username</p>
-            <input type="text" onChange={(e) => setUsername(e.target.value)} />
-          </label>
-          <label>
-            <p>Password</p>
-            <input
-              type="password"
-              onChange={(e) => setPassword(e.target.value)}
+            <p> taper votre message</p>
+            <textarea
+              type="text"
+              onChange={(e) => setMessage(e.target.value)}
             />
           </label>
-          <label>
-            <p>confirm</p>
-            <input
-              type="password"
-              onChange={(e) => setConfirm(e.target.value)}
-            />
-          </label>
-          <button className="button" onClick={handlePseudo}>
-            Se connecter
-          </button>
+          <br />
+          <button onClick={handleMessage}>Send</button>
         </form>
       </div>
-    </div>
-  
-  );
-
-  } else {
-    return (
-      <div>
-      <form>
-        <label>
-          <p>message</p>
-          <textarea type="text" 
-          onChange={(e) => setMessage(e.target.value)}/>
-        </label>
-        <br/>
-        <label>
-      channel 
-        <select  onChange={(e) => setSelect(e.target.value)}>
-          {options.map((option) => (
-            <option key={option.value} value={option.value}>
-              {option.label}
-            </option>
-          ))}
-          </select>
-
-
-
-        </label>
-          <button onClick={handleMessage}>Send</button>
-
-      </form>
+    );
+  }
+  else if(histo === true){
+    return(
+      <div className="text-center name">
+      <h1>
+      voici l'historique de {username}
+      </h1>
+      <table>
+      <tr>
+      <th>message</th>
+      <th>channel</th>
+      <th>time</th>
+      </tr>
+      {historique.map((message, index) => (
+        <tr key={index}>
+        <td>{message.message}</td>
+        <td>{message.channel}</td>
+        <td>{message.time}</td>
+        </tr>
+      ))}
+      </table>
     </div>
     )
   }
-
 }
+
 export default App;
